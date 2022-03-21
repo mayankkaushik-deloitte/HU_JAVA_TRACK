5bb41ae135b310e0878ed17a386e34366e23a6aa
Mayank Kaushik <mayakaushik187@gmail.com>
2022-03-21 14:11:30 +0530

    Moved the student class to seperate file

---
+++ src/File_Handling/Student.java
@@ -0,0 +1,32 @@
+package File_Handling;
+
+public class Student {
+    private String name, reg_no_10, reg_no_12, reg_no_btech;
+    private int Rank;
+    public Student(String name,String reg_10,String reg_12, String reg_btech, String Rank){
+        this.name = name;
+        this.reg_no_10 = reg_10;
+        this.reg_no_12 = reg_12;
+        this.reg_no_btech = reg_btech;
+        this.Rank = Integer.parseInt(Rank);
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getReg_no_10(){
+        return this.reg_no_10;
+    }
+    public String getReg_no_12(){
+        return this.reg_no_btech;
+    }
+    public String getReg_no_btech(){
+        return this.reg_no_btech;
+    }
+    public int getRank(){
+        return this.Rank;
+    }
+    @Override
+    public String toString(){
+        return this.name + " " + this.reg_no_10 + " " + this.reg_no_12 + " " + this.reg_no_btech + " " + this.Rank;
+    }
+}
